9987c11fbfa0d1dc60be22b5fe694b04a204e08e
Daniel Valencia <daniel-eve1@hotmail.com>
2019-07-12 00:43:04 -0500

    demo rompecabeza

---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        winText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Pieza.locked && Pieza2.locked && Pieza3.locked && Pieza4.locked && Pieza5.locked && Pieza6.locked)
+        winText.SetActive(true);
+    }
+}
