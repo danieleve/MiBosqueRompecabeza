032b7c8acc35e1a6db07608b3e407b7313edd2f7
danieleve <daniel-eve1@hotmail.com>
2019-08-19 23:12:31 -0500

    cambio de diseño del rompecabeza

---
+++ Assets/Script/CambioEscena.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioEscena : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CargarInfoJuego()
+    {
+        SceneManager.LoadScene("InfoJuego"); //cargar escena en específico
+    }
+
+    public void SiguienteEscena()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //carga la escena que le sigue (depende de la organización de escenas en el Build settong
+    }
+    public void EscenaAnterior()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
+
+
+
