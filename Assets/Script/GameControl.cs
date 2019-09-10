fc3b3609d8589b7fda5446e27847a3f30ade55e5
danieleve <daniel-eve1@hotmail.com>
2019-09-10 00:15:59 -0500

    cambios juego

--- Assets/Script/GameControl.cs
+++ Assets/Script/GameControl.cs
@@ -7,12 +7,17 @@ public class GameControl : MonoBehaviour
 {
 
     private Vector2 initialPosition;
+    public GameObject objectToDisable;
+    public float delayTime = 3f;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
         initialPosition = transform.position;
+
+        objectToDisable.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,11 +25,17 @@ public class GameControl : MonoBehaviour
     {
         if(Pieza.locked && Pieza2.locked && Pieza3.locked && Pieza4.locked && Pieza5.locked && Pieza6.locked)
         {
-            SceneManager.LoadScene("BienHecho");
+            objectToDisable.SetActive(true);
+            Invoke("DelayedAction", delayTime);
         }
         
            
 
+    }
+
+    void DelayedAction()
+    {
+        SceneManager.LoadScene("BienHecho");
     }
 
 
